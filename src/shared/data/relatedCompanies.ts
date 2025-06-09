5459ca4f73abaffe90555b18403cae7968efaa5f
leegeonmin2 <0101.lgm@gmail.com>
2025-06-09 17:50:12 +0900

    [건민]모달창 작성 + 메인/리스트 페이지 배경 수정

--- src/shared/data/relatedCompanies.ts
+++ src/shared/data/relatedCompanies.ts
@@ -3,14 +3,14 @@
 export const dummyRelatedCompanies = {
   keyword: "삼성전자",
   relatedCompanies: [
-    { id: "001", name: "삼성전자" },
-    { id: "002", name: "삼성반도체" },
-    { id: "003", name: "삼성위2" },
-    { id: "004", name: "삼성위3" },
-    { id: "005", name: "삼성위4" },
-    { id: "006", name: "삼성생명" },
-    { id: "007", name: "삼성디스플레이" },
-    { id: "008", name: "삼성화재" },
-    { id: "009", name: "삼성바이오로직스" },
+    { id: "001", name: "삼성전자", industry: "전자제품 제조" },
+    { id: "002", name: "삼성반도체", industry: "반도체" },
+    { id: "003", name: "삼성위2", industry: "모바일 기기" },
+    { id: "004", name: "삼성위3", industry: "디스플레이" },
+    { id: "005", name: "삼성위4", industry: "전기전자" },
+    { id: "006", name: "삼성생명", industry: "보험" },
+    { id: "007", name: "삼성디스플레이", industry: "디스플레이" },
+    { id: "008", name: "삼성화재", industry: "손해보험" },
+    { id: "009", name: "삼성바이오로직스", industry: "바이오 의약" },
   ],
 };
