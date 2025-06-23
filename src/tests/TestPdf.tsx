4525bfa467de35998e43ca00582ebaf6d702307e
jaehak26 <jack10829@naver.com>
2025-06-23 14:42:18 +0900

    [test[ html2canvas+jspdf를 사용한 pdf생성 테스트 코드 작성

--- src/tests/TestPdf.tsx
+++ src/tests/TestPdf.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import testData from './bogoser.json';
 import { PDFDownloader } from '@/tests/ReactPdf.tsx';
+import Html2JsPdf from '@/tests/Html2JsPdf.tsx';
 
 const TestPdf: React.FC = () => {
   useEffect(() => {
@@ -8,11 +9,12 @@ const TestPdf: React.FC = () => {
   }, []);
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6">PDF 테스트</h1>
-      <div className="mb-4">
-        <p className="text-gray-600 mb-2">아래 버튼을 클릭하여 PDF를 다운로드할 수 있습니다.</p>
+    <div className='p-6'>
+      <h1 className='text-2xl font-bold mb-6'>PDF 테스트</h1>
+      <div className='mb-4'>
+        <p className='text-gray-600 mb-2'>아래 버튼을 클릭하여 PDF를 다운로드할 수 있습니다.</p>
         <PDFDownloader data={testData.json} />
+        <Html2JsPdf />
       </div>
     </div>
   );
